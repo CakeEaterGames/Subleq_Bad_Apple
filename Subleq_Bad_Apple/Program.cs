1a219ef85ed0825a45b35b9482b98318139e8b8c
Ruslan Taranov <tortoed5@gmail.com>
2022-02-01 22:09:43 +1100

    Add project files.

---
+++ Subleq_Bad_Apple/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Subleq_Bad_Apple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+        }
+
+        public Program()
+        {
+            Console.WriteLine("Please enter delay in ms");
+            Console.WriteLine("(27 works best for me)");
+            int delay = int.Parse(Console.ReadLine());
+
+            StreamReader sr = new StreamReader("input.txt");
+            var code = sr.ReadToEnd();
+
+            VM vm = new VM();
+            vm.Init(code);
+
+            while (!vm.done)
+            {
+                vm.Run();
+                StringBuilder outp = new StringBuilder();
+                while (vm.outputs.Count > 0)
+                {
+                    outp.Append((char)vm.outputs.Dequeue());
+                }
+                Console.CursorLeft = 0;
+                Console.CursorTop = 0;
+                Console.Write(outp);
+                /*
+                 
+                //This is how subleq usually works
+                //but for the sake of more comfortable viewing experience 
+                //instead of feeding console inputs to the program 
+                //we feed it 0 and sleep for a couple ms
+                //and move the console pointer to reduce flickering
+
+                var inp = Console.ReadLine();
+                foreach (var c in inp)
+                {
+                    vm.inputs.Enqueue(c);
+                }
+                */
+                vm.inputs.Enqueue(0);
+                Thread.Sleep(delay);
+            }
+        }
+    }
+    
+}
+ 
---
+++ Subleq_Bad_Apple/SubleqVM.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subleq_Bad_Apple
+{
+    class VM
+    {
+        public int pc = 0;
+        public List<int> cell;
+
+        public Queue<int> inputs = new Queue<int>();
+        public Queue<int> outputs = new Queue<int>();
+
+        public bool pause = false;
+        public bool done = false;
+
+        public void Init(string inp)
+        {
+            inp = inp.Replace("\r", " ").Replace("\n", " ");
+            while (inp.Contains("  "))
+            {
+                inp = inp.Replace("  ", " ");
+            }
+            inp = inp.Trim();
+
+            cell = inp.Split(' ').Select(Int32.Parse).ToList();
+
+            pc = 0;
+            done = false;
+            pause = false;
+
+            outputs = new Queue<int>();
+            inputs = new Queue<int>();
+
+        }
+ 
+        public void Run()
+        {
+            pause = false;
+            while (!pause && !done)
+            {
+                if (pc < 0 || cell.Count < pc + 3)
+                {
+                    done = true;
+                    break;
+                }
+
+                int A = cell[pc + 0];
+                int B = cell[pc + 1];
+                int C = cell[pc + 2];
+
+                if (B == -1)
+                {
+                    outputs.Enqueue(cell[A]);
+                    pc += 3;
+                }
+                else if (A == -1)
+                {
+                    if (inputs.Count == 0)
+                    {
+                        pause = true;
+                        break;
+                    }
+                    cell[B] -= inputs.Dequeue();
+                }
+                else
+                {
+                    cell[B] -= cell[A];
+                }
+
+                if (B != -1)
+                {
+                    if (cell[B] <= 0)
+                    {
+                        pc = C;
+                    }
+                    else
+                    {
+                        pc += 3;
+                    }
+                }
+
+            }
+        }
+    }
+
+}
